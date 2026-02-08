9c6e6a9d433fd10a6c1ded3cc7649289c43ef4ed
Carlos Marques <chmarques93@gmail.com>
2026-02-07 23:56:25 -0300

    vibe code logging

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -71,6 +70,7 @@ func NewSendMessagePayload() *SendMessagePayload {
 }
 
 func setUpRabbitMQConsumer(channel *amqp.Channel) <-chan amqp.Delivery {
+	log.Printf("Declaring queue: %s", sendMessageQueueName)
 	_, err := channel.QueueDeclare(
 		sendMessageQueueName,
 		true,
@@ -81,7 +81,9 @@ func setUpRabbitMQConsumer(channel *amqp.Channel) <-chan amqp.Delivery {
 	)
 
 	failOnError(err, "Error declaring queue")
+	log.Printf("Successfully declared queue: %s", sendMessageQueueName)
 
+	log.Printf("Starting consumer on queue: %s", sendMessageQueueName)
 	messages, err := channel.Consume(
 		sendMessageQueueName,
 		"",
@@ -92,6 +94,7 @@ func setUpRabbitMQConsumer(channel *amqp.Channel) <-chan amqp.Delivery {
 		nil,
 	)
 	failOnError(err, "Error starting consumer")
+	log.Printf("Successfully started consumer on queue: %s", sendMessageQueueName)
 
 	return messages
 }
@@ -99,8 +102,12 @@ func setUpRabbitMQConsumer(channel *amqp.Channel) <-chan amqp.Delivery {
 func handleSendWhatsappMessage(client *whatsmeow.Client, body []byte) {
 	payload := NewSendMessagePayload()
 	err := json.Unmarshal(body, &payload)
-	fmt.Printf("Got a request to send message. Payload: %v", payload)
-	failOnError(err, "Error unmarshalling payload")
+	if err != nil {
+		log.Printf("ERROR: Failed to unmarshal payload: %v", err)
+		return
+	}
+	
+	log.Printf("Got a request to send message. Payload: %+v", payload)
 
 	recipientJID := types.NewJID(payload.PhoneNumber, types.DefaultUserServer)
 
@@ -110,17 +117,24 @@ func handleSendWhatsappMessage(client *whatsmeow.Client, body []byte) {
 		},
 	}
 
-	_, err = client.SendMessage(context.Background(), recipientJID, msg)
-
-	failOnError(err, "Error sending message")
+	resp, err := client.SendMessage(context.Background(), recipientJID, msg)
+	if err != nil {
+		log.Printf("ERROR: Failed to send message: %v", err)
+		return
+	}
+	
+	log.Printf("Successfully sent message. Response: %+v", resp)
 }
 
 func initRabbitMQ() *amqp.Channel {
+	log.Printf("Connecting to RabbitMQ at: %s", rabbitMQURL)
 	connection, err := amqp.Dial(rabbitMQURL)
 	failOnError(err, "Could not connect to RabbitMQ host")
+	log.Println("Successfully connected to RabbitMQ")
 
 	channel, err := connection.Channel()
 	failOnError(err, "Could not create RabbitMQ channel")
+	log.Println("Successfully created RabbitMQ channel")
 
 	return channel
 }
@@ -128,7 +142,12 @@ func initRabbitMQ() *amqp.Channel {
 func messageReceiveHandler(evt interface{}) {
 	switch evt := evt.(type) {
 	case *events.Message:
+		log.Printf("Received WhatsApp message event from: %s, IsFromMe: %v, IsGroup: %v, Type: %s", 
+			evt.Info.SenderAlt.User, evt.Info.IsFromMe, evt.Info.IsGroup, evt.Info.Type)
+		
 		if evt.Info.IsFromMe || evt.Info.IsGroup || evt.Info.Type != "text" {
+			log.Printf("Skipping message (IsFromMe: %v, IsGroup: %v, Type: %s)", 
+				evt.Info.IsFromMe, evt.Info.IsGroup, evt.Info.Type)
 			return
 		}
 
@@ -145,8 +164,12 @@ func messageReceiveHandler(evt interface{}) {
 		payload.Payload.Timestamp = evt.Info.Timestamp.Format(time.RFC3339)
 
 		msgData, err := json.Marshal(payload)
-		failOnError(err, "Error marshalling payload")
+		if err != nil {
+			log.Printf("ERROR: Failed to marshal payload: %v", err)
+			return
+		}
 
+		log.Printf("Publishing message to queue '%s': %s", receiveMessageQueueName, string(msgData))
 		err = rabbitMQChannel.Publish(
 			"",
 			receiveMessageQueueName,
@@ -157,13 +180,27 @@ func messageReceiveHandler(evt interface{}) {
 				Body:        msgData,
 			},
 		)
-		failOnError(err, "Error publishing message")
+		if err != nil {
+			log.Printf("ERROR: Failed to publish message: %v", err)
+			return
+		}
+		log.Printf("Successfully published message to queue '%s'", receiveMessageQueueName)
 	}
 }
 
 func main() {
+	log.Println("Starting WhatsApp Worker...")
+	
+	// Validate environment variables
+	if amqpHost == "" || amqpPort == "" || amqpUser == "" || amqpPassword == "" {
+		log.Fatal("Missing required environment variables: RABBITMQ_HOST, RABBITMQ_PORT, RABBITMQ_USER, RABBITMQ_PASSWORD")
+	}
+	
+	log.Printf("RabbitMQ Config - Host: %s, Port: %s, User: %s", amqpHost, amqpPort, amqpUser)
+	
 	dbLog := waLog.Stdout("Database", "DEBUG", true)
 
+	log.Println("Initializing SQLite database...")
 	container, err := sqlstore.New(context.Background(), "sqlite3", "file:storage/wadb.db?_foreign_keys=on", dbLog)
 	failOnError(err, "Error creating db container")
 
@@ -175,33 +212,56 @@ func main() {
 	client.AutomaticMessageRerequestFromPhone = true
 	client.AddEventHandler(messageReceiveHandler)
 
+	log.Println("Initializing RabbitMQ...")
 	rabbitMQChannel = initRabbitMQ()
 
 	if client.Store.ID == nil {
+		log.Println("No stored credentials found, requesting QR code...")
 		qrChan, _ := client.GetQRChannel(context.Background())
 		err = client.Connect()
 		failOnError(err, "Error connecting to client")
 
 		for evt := range qrChan {
 			if evt.Event == "code" {
+				log.Println("QR Code generated, please scan:")
 				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
 			}
 		}
 	} else {
+		log.Printf("Using stored credentials for device: %s", client.Store.ID)
 		err = client.Connect()
 		failOnError(err, "Error connecting to client")
 	}
 
+	log.Println("Setting up RabbitMQ consumer...")
 	messages := setUpRabbitMQConsumer(rabbitMQChannel)
 
+	log.Println("Starting message processing goroutine...")
 	go func() {
+		log.Println("Message processing goroutine started, waiting for messages...")
+		messageCount := 0
 		for message := range messages {
+			messageCount++
+			log.Printf("Processing message #%d from queue", messageCount)
 			handleSendWhatsappMessage(client, message.Body)
-			message.Ack(false)
+			err := message.Ack(false)
+			if err != nil {
+				log.Printf("ERROR: Failed to acknowledge message: %v", err)
+			} else {
+				log.Printf("Successfully acknowledged message #%d", messageCount)
+			}
 		}
+		log.Println("WARNING: Message channel closed!")
 	}()
+
+	log.Println("WhatsApp Worker is ready and listening for messages")
+	log.Println("Press Ctrl+C to shutdown gracefully")
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	<-c
+	
+	log.Println("Received shutdown signal, cleaning up...")
+	client.Disconnect()
+	log.Println("WhatsApp Worker stopped")
 }
